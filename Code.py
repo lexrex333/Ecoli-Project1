814b0a92bf33c890cb84cc881e5f049b062fefaf
MickyBrady <156854676+MickyBrady@users.noreply.github.com>
2025-03-30 16:53:59 -0500

    Update Code.py

--- Code.py
+++ Code.py
@@ -104,17 +104,31 @@ def multiqc(input_dir): #function to run multiqc
     #making sure this part was done
     print(f"MultiQC is done :) .")
 
-
-
-
-
-
-
 #3. Trimmomatic
 
+def run_trimmomatic(): 
+    fastqs_dir= "/home/project1/Ecoli-Project1/Fastqs"
+    output_dir= "/home/project1/Ecoli-Project1/Trimmomatic_Results"
+   
+    os.makedirs(output_dir, exist_ok=True) #make sure the output directory exsists                                    
+    for file in os.listdir(fastqs_dir): #loop through each file in the fastqs directory
+        if file.endswith(".fastq"):
+            #construct the full path to the fastq file
+            fastq_path = os.path.join(fastqs_dir, file)
+            #construct the path for the trimmed output file
+            trimmed_fastq = os.path.join(output_dir, f"trimmed_{file}")
 
+            #construct the Trimmomatic command
+            trimmomatic_command = [
+                "trimmomatic", "SE", "-phred33", fastq_path, trimmed_fastq,
+                "ILLUMINACLIP:TruSeq3-SE.fa:2:30:10", "LEADING:3", "TRAILING:3",
+                "SLIDINGWINDOW:4:15", "MINLEN:36"
+            ]
+ #run the Trimmomatic command
+            subprocess.run(trimmomatic_command, check=True)
 
-
+     #print a message indicating that Trimmomatic processing is complete for all files
+    print("Trimmomatic is done.")
 
 #4. FastQC again
 
@@ -122,3 +136,32 @@ def multiqc(input_dir): #function to run multiqc
 
 
 #5. SPAdes
+
+read_types = {} #create dictionary to hold read types (single/paired) for each genome
+
+os.chdir("Ecoli-Project1") #navigate to folder containing csv file
+
+with open("LTEE_strains.csv") as file: #access data from csv file
+    rd = csv.reader(file, delimiter=",")
+    next(file) #skip first line
+    for row in rd:
+        read_types[f"{row[6]}"] = f"{row[7]}" #add SRR numbers as keys and "single"/"paired" as values in dictionary
+
+os.chdir("Fastqs") #navigate to folder containing genomes
+
+#function to run spades given a dictionary of read numbers and single/paired read statuses
+def run_spades(read_dict):
+    for key, value in read_dict.items():
+        os.chdir(f"{key}") #navigate to folder containing the specific genome's read file(s)
+        if value == "single":
+            filename = f"{key}"+".fastq" #name of file containing reads for this genome
+            spades_command = f"spades.py -s {filename} -o {key}_assembly" #building command to run spades and output in the same folder where the reads are contained
+        elif value == "paired":
+            file1 = f"{key}"+"_1.fastq" #names of files containing reads for this genome
+            file2 = f"{key}"+"_2.fastq"
+            spades_command = f"spades.py -1 {file1} -2 {file2} -o {key}_assembly" #building command to run spades
+        os.system(spades_command) #execute command
+        os.chdir("..") #navigate back to "Fastqs" folder
+
+print(run_spades(read_types))
+print("SPAdes complete")
