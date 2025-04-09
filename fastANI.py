fff49bb41b746ff1a0b3323eb2e62431df833b80
Lexi Avalos <aavalos4@luc.edu>
2025-04-09 08:43:38 -0500

    updated fastani code

--- fastANI.py
+++ fastANI.py
@@ -9,15 +9,10 @@ main_dir = '/home/2025/aavalos4/Ecoli-Project1' #remove this when adding to pipe
 
 
 
+#FastANI
+
 #this function is going to run fastani within each pop group and compares each sample to the ancestor strain
-def fastani_run(main_dir, sample_reads, sample_csv, fastani_output="FastANI_Results", ancestor_name = "REL606"):
-    
-    
-    #THIS SECTION Will be deleted --- can grab this from previous code place where the sample reads are at
-    sample_reads = os.path.join(main_dir, 'sample_reads') 
-    #place to csv with data
-    sample_csv = os.path.join(main_dir, "sample_data.csv")
-    #--------------------
+def fastani_run(main_dir, sample_csv, fastani_output="FastANI_Results", ancestor_name = "REL606A.fasta"):
 
     #place to save fastani output
     fastani_output = os.path.join(main_dir, 'FastANI_Results')
@@ -32,13 +27,16 @@ def fastani_run(main_dir, sample_reads, sample_csv, fastani_output="FastANI_Resu
     #dictionary to hold the population groups
     pop_groups = {}
 
+    #place to put all FastANI results before putting it to csv file
+    results = [] #empty list
+
     #going through each row of the csv to group the fastas based on their groups
-    for _, row in df.iterrows():
-        sample = row["Accession"].strip() #getting accession row
+    for _, row in df.iterrows(): #_ ignores the index and just gets the rows
+        sample = row["Accession"].strip() #getting the sample name
         population = row["Population"].strip() #getting population row
         
         #path to spades fastas
-        fasta_path = os.path.join(main_dir,"sample_reads", f"{sample}_assembly", "contigs.fasta") #fasta path to spades output file
+        fasta_path = os.path.join(main_dir,"sample_assemblies", f"{sample}_assembly", "contigs.fasta") #fasta path to spades output file
         
         if os.path.isfile(fasta_path): #if there actually is a file in that path
             sample_fastas[sample] = fasta_path #add the folder name and path to the fasta file to the fastas dictionary 
@@ -52,11 +50,12 @@ def fastani_run(main_dir, sample_reads, sample_csv, fastani_output="FastANI_Resu
         else:
             print(f"No fasta for {sample} at {fasta_path}")
 
+    #debugging
+    print(list(sample_fastas.keys()))
+    print(pop_groups.keys())
+
     #Step ONE: comparing within each population
     for population, samples in pop_groups.items():
-        #making a folder for just this population
-        this_pop = os.path.join(fastani_output, f"{population}_within")
-        os.makedirs(this_pop, exist_ok=True) #add if not there already
 
         #getting all the comparisons bw 2 samples between this individual group
         #combinations(samples,2) is going to do all unique pairs without repeating them or comparing them to themselves
@@ -64,40 +63,65 @@ def fastani_run(main_dir, sample_reads, sample_csv, fastani_output="FastANI_Resu
             fasta1 = sample_fastas[sample1]
             fasta2 = sample_fastas[sample2]
 
-            #naming the output file for the pairwise comparison 
-            output_fastANI = os.path.join(this_pop, f"{sample1}_vs_{sample2}.txt") #each file is going to be named "fasta_vs_fasta.txt" to show what was compared
-
-            #fastani command
-            fastani = ["fastANI", "--query", fasta1, "--reference", fasta2, "--output", output_fastANI]
+            #fastani command -- will need to change the pathway by manually getting fastANI
+            fastani = [main_dir, "fastANI", "-q", fasta1, "-r", fasta2, "--stdout"] #storing output to terminal instead to write to csv
             #running the command
-            subprocess.run(fastani, check=True)
+            result = subprocess.run(fastani, capture_output=True, text=True, check=True)
+            #print(result)
+            
+            #going through the output and adding to the results list
+            for line in result.stdout.strip().splitlines():
+                _, _, ani, fragments, total = line.strip().split("\t") #strip whitespace and split by tab
+                results.append({
+                    "Query": sample1,
+                    "Reference": sample2,
+                    "ANI": ani,
+                    "Matching_Fragments": fragments,
+                    "Total_Fragments": total,
+                    "Group": population,
+                    "Comparison_Method": "Within Population"
+                })
 
     #STEP TWO: Comparing ALL the samples to the ancestor
     ancestor = sample_fastas.get(ancestor_name) #might have to change this later when added to pipeline
 
     if not ancestor: #if ancestor name not found in the sample list
+        #debugging
+        print(f"Ancestor {ancestor_name} not found.")
         return
     
-    #making folder for the comparisons to the ancestor outputs to go
-    ancestor_output = os.path.join(fastani_output, f"{ancestor_name}_comparisons")
-    os.makedirs(ancestor_output, exist_ok=True) #make if not there already
-
     #comparing each fasta to the ancestor except the ancestor fasta
     for sample, fasta in sample_fastas.items(): #going through sample_fastas dictionary
         if sample == ancestor_name: #if the sample equals to the ancestor
             continue #ignore and keep going
 
-        #making path to put the output txt files
-        ancestor_compare_output = os.path.join(ancestor_output, f"{sample}_vs_{ancestor_name}.txt") #naming each output file based on names for clarity
         #the fastani command
-        fastani_command = ["fastANI", "-q", fasta, "-r", ancestor, "-o", ancestor_compare_output]
+        fastani_command = [main_dir, "fastANI", "-q", fasta, "-r", ancestor, "--stdout"]
         #running the fastani command
-        subprocess.run(fastani_command, check = True)
-        
-    print("FastANI has been completed.")
+        result2 = subprocess.run(fastani_command, capture_output=True, text = True, check = True) #cleaner way of getting results
+        #print(result2)
 
-fastani_run(main_dir, sample_csv, fastani_output = "FastANI_Results", ancestor_name = "insert sample name")
+        #this looks through the csv and gets the row where the accession is equal to the sample
+        #and gets the value that is in the population column that matches that 
+        #in short talk, it is just getting the pop group that the sample belongs to
+        group = df[df["Accession"] == sample]["Population"].values[0]
 
-#need to make sure the variables match up
-#also might need to write to one file rather than multiple txt files  
-#maybe make function for the population grouping rather than just have it in this function?
+        #going through the output and adding to the results list
+        for line in result2.stdout.strip().splitlines():
+            _, _, ani, fragments, total = line.strip().split("\t") #strip whitespace and split by tab
+            results.append({"Query": sample,
+                            "Reference": ancestor_name,
+                            "ANI": ani,
+                            "Matching_Fragments": fragments,
+                            "Total_Fragments": total,
+                            "Group": group,
+                            "Comparison_Method": "To Ancestor"
+                            })
+    
+    results_to_df = pd.DataFrame(results) #changing the results to a dataframe
+    results_to_df.to_csv(os.path.join(fastani_output, "FastANI_Results_ALL.csv"), index = False)
+    
+    print("FastANI has been completed!!")
+
+fastani_run(main_dir, sample_csv = os.path.join(main_dir, "sample_data.csv"), fastani_output = "FastANI_Results", 
+            ancestor_name = "SRR22764941")
